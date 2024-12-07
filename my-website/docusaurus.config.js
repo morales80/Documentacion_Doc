d14de5dedf551e0633f18e9e292200ae67c67216
Lu15R <ruberrodriguezcarlos@gmail.com>
2024-12-07 12:30:53 -0600

    update docusaurus.config
    actualize config docusaurus

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -22,8 +22,9 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Lu15R',
+  projectName: 'Documentacion_Doc',
+  
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
